962c40b95484c7f34250d9a2f6eb06abedada4b0
Elad <katzelad@gmail.com>
2017-02-13 20:07:10 +0200

    simplify

--- extract.py
+++ extract.py
@@ -2,6 +2,13 @@ import sklearn as sk, numpy as np, scipy as sp, dateutil.parser, time, parse
 
 class C:
     LABEL, TS, TRAJ, X, Y, Z = tuple(range(0, 6))
+    
+def append_stats(traj_features, features):
+    features.append(np.min(traj_features))
+    features.append(np.max(traj_features))
+    features.append(np.median(traj_features))
+    features.append(np.mean(traj_features))
+    features.append(np.std(traj_features))
 
 def traj2features(samples):
     np_samples = np.array(samples)
@@ -12,25 +19,10 @@ def traj2features(samples):
     ys = np_samples[:, C.Y]
     zs = np_samples[:, C.Z]
     
-    features.append(np.min(timestamps))
-    features.append(np.max(timestamps))
-    features.append(np.median(timestamps))
-    features.append(np.mean(timestamps))
-    
-    features.append(np.min(xs))
-    features.append(np.max(xs))
-    features.append(np.median(xs))
-    features.append(np.mean(xs))
-    
-    features.append(np.min(ys))
-    features.append(np.max(ys))
-    features.append(np.median(ys))
-    features.append(np.mean(ys))
-    
-    features.append(np.min(zs))
-    features.append(np.max(zs))
-    features.append(np.median(zs))
-    features.append(np.mean(zs))
+    append_stats(timestamps, features)
+    append_stats(xs, features)
+    append_stats(ys, features)
+    append_stats(zs, features)
     
 #     features.append(...)
     return features
